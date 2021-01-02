19d8432959ccca92c26790293b82ef10739d1b6f
Andy Jones <26009223+jonesandy@users.noreply.github.com>
2021-01-02 23:38:38 +0000

    Added Book class.

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        private List<double> grades;
+        private string name;
+        private double total = 0.0;
+        private double highGrade = double.MinValue;
+        private double lowGrade = double.MaxValue;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void ShowStatistics()
+        {
+            ComputeStatistics();
+            PrintStatistics();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        private void ComputeStatistics()
+        {
+            foreach (var score in grades)
+            {
+                highGrade = Math.Max(score, highGrade);
+                lowGrade = Math.Min(score, lowGrade);
+                total += score;
+            }
+        }
+
+        private void PrintStatistics()
+        {
+            double average = total / grades.Count;
+
+            Console.WriteLine($"Statistics for GradeBook - {name}");
+            Console.WriteLine($"The tallied score for all grades: {total:N1}");
+            Console.WriteLine($"The average grade: {average:N1}");
+            Console.WriteLine($"The lowest grade was: {lowGrade:N1}");
+            Console.WriteLine($"The highest grade was: {highGrade:N1}");
+        }
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,30 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace GradeBook
+﻿namespace GradeBook
 {
     class Program
     {
         static void Main()
         {
-            var total = 0.0;
-            var grades = new List<double>()
-            {
-                87.9,
-                75.3,
-                86.3,
-                92.1
-            };
+            var book = new Book("Andy");
+            book.AddGrade(87.9);
+            book.AddGrade(75.3);
+            book.AddGrade(86.3);
+            book.AddGrade(92.1);
 
-            foreach(var score in grades)
-            {
-                total += score;
-            }
-
-            double average = total / grades.Count;
-
-            Console.WriteLine($"The tallied score for all grades: {total:N1}");
-            Console.WriteLine($"The average grade: {average:N1}");
+            book.ShowStatistics();
         }
     }
 }
