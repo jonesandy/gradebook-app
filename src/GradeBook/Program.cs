44c9eca65600e046287ffa38c2b07c1a99fbc153
Andy Jones <26009223+jonesandy@users.noreply.github.com>
2021-01-02 18:32:36 +0000

    Initial program file.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main()
+        {
+            var total = 0.0;
+            var grades = new List<double>()
+            {
+                87.9,
+                75.3,
+                86.3,
+                92.1
+            };
+
+            foreach(var score in grades)
+            {
+                total += score;
+            }
+
+            double average = total / grades.Count;
+
+            Console.WriteLine($"The tallied score for all grades: {total:N1}");
+            Console.WriteLine($"The average grade: {average:N1}");
+        }
+    }
+}
